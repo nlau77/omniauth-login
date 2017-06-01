588a9dc2147ddc32f52400fe28a64f105e128837
nlau77 <nlau777@gmail.com>
2017-05-31 21:42:54 -0400

    set up routes for sign in sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
 
-  # get "auth/:provider/callback", to: "sessions#create"
-
+  get "auth/:provider/callback", to: "sessions#create"
+  resources :auth, only: :show
+  resources :sessions, only: [:new, :create]
+  
 end
